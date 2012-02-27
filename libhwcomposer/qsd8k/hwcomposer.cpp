4c77815bb56d932f5707a5dd098325fa39e065f8
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-02-26 23:20:15 -0600

    qsd8k-hwcomposer: update drawLayerUsingCopybit, revise prepare/set

--- libhwcomposer/qsd8k/hwcomposer.cpp
+++ libhwcomposer/qsd8k/hwcomposer.cpp
@@ -54,10 +54,10 @@ struct hwc_context_t {
 };
 
 static int hwc_device_open(const struct hw_module_t* module, const char* name,
-                           struct hw_device_t** device);
+        struct hw_device_t** device);
 
 static struct hw_module_methods_t hwc_module_methods = {
-open: hwc_device_open
+    open: hwc_device_open
 };
 
 struct private_hwc_module_t {
@@ -68,20 +68,20 @@ struct private_hwc_module_t {
 };
 
 struct private_hwc_module_t HAL_MODULE_INFO_SYM = {
-base: {
-common: {
-tag: HARDWARE_MODULE_TAG,
-version_major: 1,
-version_minor: 0,
-id: HWC_HARDWARE_MODULE_ID,
-name: "Hardware Composer Module",
-author: "The Android Open Source Project",
-methods: &hwc_module_methods,
-}
-},
-copybitEngine: NULL,
-fbDevice: NULL,
-compositionType: 0,
+    base: {
+        common: {
+            tag: HARDWARE_MODULE_TAG,
+            version_major: 1,
+            version_minor: 0,
+            id: HWC_HARDWARE_MODULE_ID,
+            name: "Hardware Composer Module",
+            author: "The Android Open Source Project",
+            methods: &hwc_module_methods,
+        }
+   },
+   copybitEngine: NULL,
+   fbDevice: NULL,
+   compositionType: 0,
 };
 
 /*****************************************************************************/
@@ -100,43 +100,41 @@ static void dump_layer(hwc_layer_t const* l) {
 }
 
 static int hwc_prepare(hwc_composer_device_t *dev, hwc_layer_list_t* list) {
-    
+
     // if there is no list or geometry is not changed,
     // there is no need to do any work here
     if( !list || (!(list->flags & HWC_GEOMETRY_CHANGED)))
         return 0;
-    
+
     private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>(
-                                                                              dev->common.module);
-    
+                                                           dev->common.module);
+
     if (!hwcModule) {
         LOGE("hwc_prepare invalid module");
         return -1;
     }
-    
+
     //TODO: this probably needs work
     LOGD_IF(HWC_DEBUG,"hwc_prepare found %d layers",list->numHwLayers);
     for (size_t i=0 ; i<list->numHwLayers ; i++) {
         dump_layer(&list->hwLayers[i]);
-        
+
         // check for skip layer
         if (list->hwLayers[i].flags & HWC_SKIP_LAYER) {
             LOGD_IF(HWC_DEBUG,"hwc_prepare HWC_SKIP_LAYER on layer %d",i);
-            if (hwcModule->compositionType & COMPOSITION_TYPE_MDP) {
-                // ensure that HWC_OVERLAY layers below skip layers do not
-                // overwrite GPU composed skip layers.
-                ssize_t jj = ((ssize_t)i) - 1;
-                while (jj >= 0) {
-                    // re-mark every overlay layer below the
-                    // skip-layer for GPU composition.
-                    LOGD_IF(HWC_DEBUG,"hwc_prepare using framebuffer for layer %d",jj);
-                    list->hwLayers[jj].compositionType = HWC_FRAMEBUFFER;
-                    jj--;
-                }
+            ssize_t layer_countdown = ((ssize_t)i) - 1;
+            // Mark every layer below the SKIP layer to be composed by the GPU
+            while (layer_countdown >= 0)
+            {
+                list->hwLayers[layer_countdown].compositionType = HWC_FRAMEBUFFER;
+                list->hwLayers[layer_countdown].hints &= ~HWC_HINT_CLEAR_FB;
+                layer_countdown--;
             }
             continue;
         }
-        
+
+
+
         // use copybit for everything
         if (hwcModule->compositionType & COMPOSITION_TYPE_MDP) {
             LOGD_IF(HWC_DEBUG,"hwc_prepare using copybit for layer %d", i);
@@ -156,17 +154,17 @@ struct range {
 };
 
 struct region_iterator : public copybit_region_t {
-    
+
     region_iterator(hwc_region_t region) {
         mRegion = region;
         r.end = region.numRects;
         r.current = 0;
         this->next = iterate;
     }
-    
+
 private:
     static int iterate(copybit_region_t const * self, copybit_rect_t* rect) {
-        
+
         if (!self) {
             LOGE("iterate invalid copybit region");
             return 0;
@@ -174,7 +172,7 @@ private:
             LOGE("iterate invalid copybit rect");
             return 0;
         }
-        
+
         region_iterator const* me = static_cast<region_iterator const*>(self);
         if (me->r.current != me->r.end) {
             rect->l = me->mRegion.rects[me->r.current].left;
@@ -186,34 +184,34 @@ private:
         }
         return 0;
     }
-    
+
     hwc_region_t mRegion;
     mutable range r;
 };
 
 static int drawLayerUsingCopybit(hwc_composer_device_t *dev,
-                                 hwc_layer_t *layer,
-                                 EGLDisplay dpy,
-                                 EGLSurface surface)
+                                hwc_layer_t *layer,
+                                EGLDisplay dpy,
+                                EGLSurface surface)
 {
     hwc_context_t* ctx = (hwc_context_t*)(dev);
     if(!ctx) {
-        LOGE("drawLayerUsingCopybit null context ");
-        return -1;
+         LOGE("drawLayerUsingCopybit null context ");
+         return -1;
     }
-    
+
     private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>(dev->common.module);
     if(!hwcModule) {
         LOGE("drawLayerUsingCopybit null module ");
         return -1;
     }
-    
+
     private_handle_t *hnd = (private_handle_t *)layer->handle;
     if(!hnd) {
         LOGE("drawLayerUsingCopybit invalid handle");
         return -1;
     }
-    
+
     // Lock this buffer for read.
     genlock_lock_type lockType = GENLOCK_READ_LOCK;
     int err = genlock_lock_buffer(hnd, lockType, GENLOCK_MAX_TIMEOUT);
@@ -237,7 +235,7 @@ static int drawLayerUsingCopybit(hwc_composer_device_t *dev,
     int alignment = 32;
     if( HAL_PIXEL_FORMAT_RGB_565 == fbHandle->format )
         alignment = 16;
-    // Set the copybit source:
+     // Set the copybit source:
     copybit_image_t src;
     src.w = ALIGN(hnd->width, alignment);
     src.h = hnd->height;
@@ -249,19 +247,19 @@ static int drawLayerUsingCopybit(hwc_composer_device_t *dev,
     // this needs to change to accomodate vertical stride
     // if needed in the future
     src.vert_padding = 0;
-    
+
     // Copybit source rect
     hwc_rect_t sourceCrop = layer->sourceCrop;
     copybit_rect_t srcRect = {sourceCrop.left, sourceCrop.top,
-        sourceCrop.right,
-        sourceCrop.bottom};
-    
+                              sourceCrop.right,
+                              sourceCrop.bottom};
+
     // Copybit destination rect
     hwc_rect_t displayFrame = layer->displayFrame;
     copybit_rect_t dstRect = {displayFrame.left, displayFrame.top,
-        displayFrame.right,
-        displayFrame.bottom};
-    
+                              displayFrame.right,
+                              displayFrame.bottom};
+
     // Copybit dst
     copybit_image_t dst;
     dst.w = ALIGN(fbHandle->width,alignment);
@@ -269,12 +267,84 @@ static int drawLayerUsingCopybit(hwc_composer_device_t *dev,
     dst.format = fbHandle->format;
     dst.base = (void *)fbHandle->base;
     dst.handle = (native_handle_t *)renderBuffer->handle;
-    
+
+    copybit_device_t *copybit = hwcModule->copybitEngine;
+
+    int32_t screen_w        = displayFrame.right - displayFrame.left;
+    int32_t screen_h        = displayFrame.bottom - displayFrame.top;
+    int32_t src_crop_width  = sourceCrop.right - sourceCrop.left;
+    int32_t src_crop_height = sourceCrop.bottom -sourceCrop.top;
+
+    int32_t copybitsMaxScale = copybit->get(copybit,COPYBIT_MAGNIFICATION_LIMIT);
+
+    if(layer->transform & (HWC_TRANSFORM_ROT_90 | HWC_TRANSFORM_ROT_270)){
+        //swap screen width and height
+        int tmp = screen_w;
+        screen_w  = screen_h;
+        screen_h = tmp;
+    }
+    int32_t dsdx = screen_w/src_crop_width;
+    int32_t dtdy = screen_h/src_crop_height;
+    sp<GraphicBuffer> tempBitmap;
+
+    if(dsdx  > copybitsMaxScale || dtdy > copybitsMaxScale){
+        // The requested scale is out of the range the hardware
+        // can support.
+       LOGD("%s:%d::Need to scale dsdx=%d, dtdy=%d,maxScaleInv=%d,screen_w=%d,screen_h=%d \
+                  src_crop_width=%d src_crop_height=%d",__FUNCTION__,__LINE__,
+                  dsdx,dtdy,copybitsMaxScale,screen_w,screen_h,src_crop_width,src_crop_height);
+
+       //Driver makes width and height as even
+       //that may cause wrong calculation of the ratio
+       //in display and crop.Hence we make
+       //crop width and height as even.
+       src_crop_width  = (src_crop_width/2)*2;
+       src_crop_height = (src_crop_height/2)*2;
+
+       int tmp_w =  src_crop_width*copybitsMaxScale;
+       int tmp_h =  src_crop_height*copybitsMaxScale;
+
+       LOGD("%s:%d::tmp_w = %d,tmp_h = %d",__FUNCTION__,__LINE__,tmp_w,tmp_h);
+       tempBitmap = new GraphicBuffer(
+                    tmp_w, tmp_h, src.format,
+                    GraphicBuffer::USAGE_HW_2D);
+
+       err = tempBitmap->initCheck();
+       if (err == android::NO_ERROR){
+            copybit_image_t tmp_dst;
+            copybit_rect_t tmp_rect;
+            tmp_dst.w = tmp_w;
+            tmp_dst.h = tmp_h;
+            tmp_dst.format = tempBitmap->format;
+            tmp_dst.handle = (native_handle_t*)tempBitmap->getNativeBuffer()->handle;
+            tmp_dst.horiz_padding = src.horiz_padding;
+            tmp_dst.vert_padding = src.vert_padding;
+            tmp_rect.l = 0;
+            tmp_rect.t = 0;
+            tmp_rect.r = tmp_dst.w;
+            tmp_rect.b = tmp_dst.h;
+            //create one clip region
+            hwc_rect tmp_hwc_rect = {0,0,tmp_rect.r,tmp_rect.b};
+            hwc_region_t tmp_hwc_reg = {1,(hwc_rect_t const*)&tmp_hwc_rect};
+            region_iterator tmp_it(tmp_hwc_reg);
+            copybit->set_parameter(copybit,COPYBIT_TRANSFORM,0);
+            copybit->set_parameter(copybit, COPYBIT_PLANE_ALPHA,
+                        (layer->blending == HWC_BLENDING_NONE) ? -1 : layer->alpha);
+            err = copybit->stretch(copybit,&tmp_dst, &src, &tmp_rect, &srcRect, &tmp_it);
+            if(err < 0){
+                LOGE("%s:%d::tmp copybit stretch failed",__FUNCTION__,__LINE__);
+                return err;
+            }
+            // copy new src and src rect crop
+            src = tmp_dst;
+            srcRect = tmp_rect;
+      }
+    }
+
     // Copybit region
     hwc_region_t region = layer->visibleRegionScreen;
     region_iterator copybitRegion(region);
-    
-    copybit_device_t *copybit = hwcModule->copybitEngine;
+
     copybit->set_parameter(copybit, COPYBIT_FRAMEBUFFER_WIDTH, renderBuffer->width);
     copybit->set_parameter(copybit, COPYBIT_FRAMEBUFFER_HEIGHT, renderBuffer->height);
     copybit->set_parameter(copybit, COPYBIT_TRANSFORM, layer->transform);
@@ -282,27 +352,29 @@ static int drawLayerUsingCopybit(hwc_composer_device_t *dev,
                            (layer->blending == HWC_BLENDING_NONE) ? -1 : layer->alpha);
     copybit->set_parameter(copybit, COPYBIT_PREMULTIPLIED_ALPHA,
                            (layer->blending == HWC_BLENDING_PREMULT)? COPYBIT_ENABLE : COPYBIT_DISABLE);
+    copybit->set_parameter(copybit, COPYBIT_DITHER,
+                            (dst.format == HAL_PIXEL_FORMAT_RGB_565)? COPYBIT_ENABLE : COPYBIT_DISABLE);
     err = copybit->stretch(copybit, &dst, &src, &dstRect, &srcRect, &copybitRegion);
-    
+
     if(err < 0)
         LOGE("drawLayerUsingCopybit stretch failed");
-    
+
     // Unlock this buffer since copybit is done with it.
     err = genlock_unlock_buffer(hnd);
     if (GENLOCK_FAILURE == err) {
         LOGE("drawLayerUsingCopybit genlock_unlock_buffer failed");
     }
-    
+
     LOGD_IF(HWC_DEBUG_COPYBIT,"drawLayerUsingCopybit completed with err %d",err);
     return err;
 }
 
 static int hwc_set(hwc_composer_device_t *dev,
-                   hwc_display_t dpy,
-                   hwc_surface_t sur,
-                   hwc_layer_list_t* list)
+        hwc_display_t dpy,
+        hwc_surface_t sur,
+        hwc_layer_list_t* list)
 {
-    
+
     if (dpy == NULL && sur == NULL && list == NULL) {
         // special case: the screen is off, there is nothing to do.
         LOGD_IF(HWC_DEBUG,"hwc_set screen is off");
@@ -317,29 +389,29 @@ static int hwc_set(hwc_composer_device_t *dev,
         }
         return 0;
     }
-    
+
     private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>(
-                                                                              dev->common.module);
-    
+                                                           dev->common.module);
+
     if (!hwcModule) {
         LOGE("hwc_set invalid module");
         return -1;
     }
-    
-    framebuffer_device_t *fbDev = hwcModule->fbDevice;
-    
-    //TODO: this probably needs work
+
     for (size_t i=0; i<list->numHwLayers; i++) {
-        dump_layer(&list->hwLayers[i]);
         if (list->hwLayers[i].flags & HWC_SKIP_LAYER) {
             continue;
         } else if (list->flags & HWC_SKIP_COMPOSITION) {
-            break;
+            continue;
         } else if (list->hwLayers[i].compositionType == HWC_USE_COPYBIT) {
-            drawLayerUsingCopybit(dev, &(list->hwLayers[i]), (EGLDisplay)dpy, (EGLSurface)sur);
+            drawLayerUsingCopybit(dev, &(list->hwLayers[i]),
+                                    (EGLDisplay)dpy, (EGLSurface)sur);
         }
     }
-    
+
+    if(list->flags & HWC_SKIP_COMPOSITION)
+        LOGD_IF(HWC_DEBUG,"hwc_set skipping eglSwapBuffer call");
+
     // Do not call eglSwapBuffers if the skip composition flag is set on the list.
     if (!(list->flags & HWC_SKIP_COMPOSITION)) {
         EGLBoolean sucess = eglSwapBuffers((EGLDisplay)dpy, (EGLSurface)sur);
@@ -348,7 +420,7 @@ static int hwc_set(hwc_composer_device_t *dev,
             return HWC_EGL_ERROR;
         }
     }
-    
+
     return 0;
 }
 
@@ -358,11 +430,11 @@ static int hwc_device_close(struct hw_device_t *dev)
         LOGE("hwc_device_close null device pointer");
         return -1;
     }
-    
+
     struct hwc_context_t* ctx = (struct hwc_context_t*)dev;
-    
+
     private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>(
-                                                                              ctx->device.common.module);
+            ctx->device.common.module);
     // Close the copybit module
     if(hwcModule->copybitEngine) {
         copybit_close(hwcModule->copybitEngine);
@@ -372,7 +444,7 @@ static int hwc_device_close(struct hw_device_t *dev)
         framebuffer_close(hwcModule->fbDevice);
         hwcModule->fbDevice = NULL;
     }
-    
+
     if (ctx) {
         free(ctx);
     }
@@ -382,7 +454,7 @@ static int hwc_device_close(struct hw_device_t *dev)
 /*****************************************************************************/
 static int hwc_module_initialize(struct private_hwc_module_t* hwcModule)
 {
-    
+
     // Open the copybit module
     hw_module_t const *module;
     if (hw_get_module(COPYBIT_HARDWARE_MODULE_ID, &module) == 0) {
@@ -391,7 +463,7 @@ static int hwc_module_initialize(struct private_hwc_module_t* hwcModule)
     if (hw_get_module(GRALLOC_HARDWARE_MODULE_ID, &module) == 0) {
         framebuffer_open(module, &(hwcModule->fbDevice));
     }
-    
+
     // get the current composition type
     char property[PROPERTY_VALUE_MAX];
     if (property_get("debug.sf.hw", property, NULL) > 0) {
@@ -408,42 +480,42 @@ static int hwc_module_initialize(struct private_hwc_module_t* hwcModule)
             } else {
                 hwcModule->compositionType = COMPOSITION_TYPE_GPU;
             }
-            
+
             if(!hwcModule->copybitEngine)
                 hwcModule->compositionType = COMPOSITION_TYPE_GPU;
         }
     } else { //debug.sf.hw is not set. Use cpu composition
         hwcModule->compositionType = COMPOSITION_TYPE_CPU;
     }
-    
+
     return 0;
 }
 
 static int hwc_device_open(const struct hw_module_t* module, const char* name,
-                           struct hw_device_t** device)
+        struct hw_device_t** device)
 {
     int status = -EINVAL;
     if (!strcmp(name, HWC_HARDWARE_COMPOSER)) {
         private_hwc_module_t* hwcModule = reinterpret_cast<private_hwc_module_t*>
-        (const_cast<hw_module_t*>(module));
-        
+                                        (const_cast<hw_module_t*>(module));
+
         hwc_module_initialize(hwcModule);
-        
+
         struct hwc_context_t *dev;
         dev = (hwc_context_t*)malloc(sizeof(*dev));
-        
+
         /* initialize our state here */
         memset(dev, 0, sizeof(*dev));
-        
+
         /* initialize the procs */
         dev->device.common.tag = HARDWARE_DEVICE_TAG;
         dev->device.common.version = 0;
         dev->device.common.module = const_cast<hw_module_t*>(module);
         dev->device.common.close = hwc_device_close;
-        
+
         dev->device.prepare = hwc_prepare;
         dev->device.set = hwc_set;
-        
+
         *device = &dev->device.common;
         status = 0;
     }
